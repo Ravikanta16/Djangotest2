7f7a106714a9bbca9391852125b00819ced59bcd
Ravikanta16 <ravikantagarg1999@gmail.com>
2023-06-16 23:38:40 +0530

    updated template

--- intro/views.py
+++ intro/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
+"""def home(request):
     return HttpResponse("Hello")
 
 def room(request):
-    return HttpResponse("hy")
+    return HttpResponse("hy")"""
+
+def home(request):
+    return render(request, 'home.html')
+
+def room(request):
+    return render(request, 'room.html')
 
