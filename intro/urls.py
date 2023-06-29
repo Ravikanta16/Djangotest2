3e86513b6f8d9dca0611264dba8a78648ca06645
Ravikanta16 <ravikantagarg1999@gmail.com>
2023-06-29 14:08:37 +0530

    Before the database

--- intro/urls.py
+++ intro/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-    path('room/', views.room, name='room'),
+    #path('room/', views.room, name='room'),
+    path('room/<str:pk>/', views.room, name='room'),
 ]
--- intro/views.py
+++ intro/views.py
@@ -8,9 +8,41 @@ from django.http import HttpResponse
 def room(request):
     return HttpResponse("hy")"""
 
-def home(request):
+
+"""def home(request):
     return render(request, 'home.html')
 
 def room(request):
-    return render(request, 'room.html')
+    return render(request, 'room.html')"""
 
+
+Details = [
+    {'id':1,  'name':'Ravikanta!'},
+    {'id':2,  'name':'Megha'},
+    {'id':3,  'name':'Juhi'},
+]
+
+"""def home(request):
+    context={'Details':Details}
+    return render(request, 'home.html',context)
+
+def room(request):
+    return render(request, 'room.html')"""
+
+def home(request):
+    context={'Details':Details}
+    return render(request, 'intro/home1.html',context)
+
+#def room(request):
+    #return render(request, 'intro/room1.html')
+
+#def room(request, pk):
+    #return render(request, 'intro/room1.html')
+
+def room(request, pk):
+    detail=None
+    for i in Details:
+        if i['id'] == int(pk):
+            detail=i
+    context={'detail': detail}   
+    return render(request, 'intro/room1.html',context)
